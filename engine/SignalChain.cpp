cba2d8d1a40bc6e29a091faf8ac041e1661571cd
puyan3939 <puyan3939@gmail.com>
2026-08-18 17:52:30 +0900

    route cab mic between amp and post effects

--- engine/SignalChain.cpp
+++ engine/SignalChain.cpp
@@ -14,6 +14,8 @@ void SignalChain::prepare(double sampleRate, int maximumBlockSize)
     ampEngine.prepare(sampleRate, maximumBlockSize);
     hqAmpEngine.prepare(sampleRate, maximumBlockSize);
     hqEffects.prepare(sampleRate, maximumBlockSize);
+    cabMic.prepare(sampleRate, maximumBlockSize);
+    cabMic.setEnabled(false);
     limiter.prepare(sampleRate);
     limiter.setCeiling(0.28f);
     limiter.setOutputGainDb(-18.0f);
@@ -24,6 +26,7 @@ void SignalChain::reset()
     ampEngine.reset();
     hqAmpEngine.reset();
     hqEffects.reset();
+    cabMic.reset();
     limiter.reset();
     ampWorkBuffer.clear();
 }
@@ -41,10 +44,9 @@ void SignalChain::process(juce::AudioBuffer<float>& buffer, int startSample, int
     {
         applyInputGain(buffer, startSample, numSamples);
         hqEffects.processPreAmp(buffer, startSample, numSamples);
-        if (getAmpMode() == AmpMode::hq)
-            processHQAmp(buffer, startSample, numSamples);
-        else
-            processLegacyAmp(buffer, startSample, numSamples);
+        if (getAmpMode() == AmpMode::hq) processHQAmp(buffer, startSample, numSamples);
+        else processLegacyAmp(buffer, startSample, numSamples);
+        cabMic.process(buffer, startSample, numSamples);
         hqEffects.processPostAmp(buffer, startSample, numSamples);
     }
 
@@ -56,11 +58,7 @@ void SignalChain::copyDetectedMonoToStereo(juce::AudioBuffer<float>& buffer, int
     auto* left = buffer.getWritePointer(0, startSample);
     auto* right = buffer.getWritePointer(1, startSample);
     float peakLeft = 0.0f, peakRight = 0.0f;
-    for (int i = 0; i < numSamples; ++i)
-    {
-        peakLeft = juce::jmax(peakLeft, std::abs(left[i]));
-        peakRight = juce::jmax(peakRight, std::abs(right[i]));
-    }
+    for (int i = 0; i < numSamples; ++i){peakLeft = juce::jmax(peakLeft, std::abs(left[i]));peakRight = juce::jmax(peakRight, std::abs(right[i]));}
     const bool sourceIsRight = peakRight > peakLeft;
     auto* source = sourceIsRight ? right : left;
     auto* destination = sourceIsRight ? left : right;
@@ -71,48 +69,25 @@ void SignalChain::applyInputGain(juce::AudioBuffer<float>& buffer, int startSamp
 {
     inputGain.setTargetValue(juce::Decibels::decibelsToGain(inputGainDb.load(std::memory_order_relaxed)));
     const int channels = juce::jmin(2, buffer.getNumChannels());
-    for (int i = 0; i < numSamples; ++i)
-    {
-        const float gain = inputGain.getNextValue();
-        for (int ch = 0; ch < channels; ++ch)
-            buffer.getWritePointer(ch, startSample)[i] *= gain;
-    }
+    for (int i = 0; i < numSamples; ++i){const float gain = inputGain.getNextValue();for (int ch = 0; ch < channels; ++ch) buffer.getWritePointer(ch, startSample)[i] *= gain;}
 }
 
 void SignalChain::processLegacyAmp(juce::AudioBuffer<float>& buffer, int startSample, int numSamples)
 {
-    ampWorkBuffer.setSize(1, numSamples, false, false, true);
-    ampWorkBuffer.copyFrom(0, 0, buffer, 0, startSample, numSamples);
-    ampEngine.process(ampWorkBuffer);
-
-    const int channels = juce::jmin(2, buffer.getNumChannels());
-    for (int ch = 0; ch < channels; ++ch)
-        buffer.copyFrom(ch, startSample, ampWorkBuffer, 0, 0, numSamples);
+    ampWorkBuffer.setSize(1, numSamples, false, false, true);ampWorkBuffer.copyFrom(0, 0, buffer, 0, startSample, numSamples);ampEngine.process(ampWorkBuffer);
+    const int channels = juce::jmin(2, buffer.getNumChannels());for (int ch = 0; ch < channels; ++ch) buffer.copyFrom(ch, startSample, ampWorkBuffer, 0, 0, numSamples);
 }
 
 void SignalChain::processHQAmp(juce::AudioBuffer<float>& buffer, int startSample, int numSamples)
 {
-    ampWorkBuffer.setSize(1, numSamples, false, false, true);
-    ampWorkBuffer.copyFrom(0, 0, buffer, 0, startSample, numSamples);
-    hqAmpEngine.process(ampWorkBuffer);
-
-    const int channels = juce::jmin(2, buffer.getNumChannels());
-    for (int ch = 0; ch < channels; ++ch)
-        buffer.copyFrom(ch, startSample, ampWorkBuffer, 0, 0, numSamples);
+    ampWorkBuffer.setSize(1, numSamples, false, false, true);ampWorkBuffer.copyFrom(0, 0, buffer, 0, startSample, numSamples);hqAmpEngine.process(ampWorkBuffer);
+    const int channels = juce::jmin(2, buffer.getNumChannels());for (int ch = 0; ch < channels; ++ch) buffer.copyFrom(ch, startSample, ampWorkBuffer, 0, 0, numSamples);
 }
 
 void SignalChain::applyStartupFadeAndLimiter(juce::AudioBuffer<float>& buffer, int startSample, int numSamples)
 {
     const int channels = juce::jmin(2, buffer.getNumChannels());
-    for (int i = 0; i < numSamples; ++i)
-    {
-        const float fade = startupFade.getNextValue();
-        for (int ch = 0; ch < channels; ++ch)
-        {
-            float& sample = buffer.getWritePointer(ch, startSample)[i];
-            sample = limiter.processSample(sample * fade);
-        }
-    }
+    for (int i = 0; i < numSamples; ++i){const float fade = startupFade.getNextValue();for (int ch = 0; ch < channels; ++ch){float& sample = buffer.getWritePointer(ch, startSample)[i];sample = limiter.processSample(sample * fade);}}
 }
 
 void SignalChain::setInputGainDb(float gainDb) noexcept { inputGainDb.store(juce::jlimit(-36.0f, 18.0f, gainDb), std::memory_order_relaxed); }
