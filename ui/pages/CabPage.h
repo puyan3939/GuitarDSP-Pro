e7b891b95c86364d6369c39ee6604429f8a70058
puyan3939 <puyan3939@gmail.com>
2026-08-18 17:53:00 +0900

    add cab mic controls UI

--- ui/pages/CabPage.h
+++ ui/pages/CabPage.h
@@ -1,15 +1,19 @@
 #pragma once
-
 #include <JuceHeader.h>
+#include "../../hq_preload/dsp/cab/CabMicEngineHQ.h"
 
 class CabPage : public juce::Component
 {
 public:
-    CabPage();
+    explicit CabPage(guitardsp::hq::CabMicEngineHQ& engine);
     void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    juce::Label title;
-    juce::Label info;
+    void push();
+    guitardsp::hq::CabMicEngineHQ& cab;
+    juce::Label title, info;
+    juce::ToggleButton enabled { "CAB / MIC ON" };
+    juce::ComboBox cabType, micType;
+    juce::Slider position, distance, resonance, lowCut, highCut, mix;
 };
