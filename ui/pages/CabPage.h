c66dffe6bcd8f144e702e5056f88cbeb18fcceff
puyan3939 <puyan3939@gmail.com>
2026-08-18 23:54:38 +0900

    expose cab UI refresh for preset loading

--- ui/pages/CabPage.h
+++ ui/pages/CabPage.h
@@ -8,6 +8,19 @@ public:
     explicit CabPage(guitardsp::hq::CabMicEngineHQ& engine);
     void paint(juce::Graphics&) override;
     void resized() override;
+    void refreshFromEngine()
+    {
+        const auto& p=cab.getParameters();
+        enabled.setToggleState(cab.isEnabled(),juce::dontSendNotification);
+        cabType.setSelectedId((int)p.cab+1,juce::dontSendNotification);
+        micType.setSelectedId((int)p.mic+1,juce::dontSendNotification);
+        position.setValue(p.position,juce::dontSendNotification);
+        distance.setValue(p.distance,juce::dontSendNotification);
+        resonance.setValue(p.resonance,juce::dontSendNotification);
+        lowCut.setValue(p.lowCutHz,juce::dontSendNotification);
+        highCut.setValue(p.highCutHz,juce::dontSendNotification);
+        mix.setValue(p.mix,juce::dontSendNotification);
+    }
 
 private:
     void push();
