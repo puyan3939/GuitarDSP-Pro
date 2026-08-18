29e86aea35093afc8b670be884088feef6799709
puyan3939 <puyan3939@gmail.com>
2026-08-18 20:07:54 +0900

    wire FX page to HQ rack

--- ui/pages/EffectsPage.h
+++ ui/pages/EffectsPage.h
@@ -1,15 +1,36 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "../../hq_preload/dsp/HQEffectsRack.h"
 
 class EffectsPage : public juce::Component
 {
 public:
-    EffectsPage();
+    explicit EffectsPage(guitardsp::hq::HQEffectsRack& rack);
     void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
+    void pushMod();
+    void pushDelay();
+    void pushReverb();
+    void updateModeHints();
+    void setupKnob(juce::Slider& s, double lo, double hi, double value, double step, const juce::String& suffix = {});
+
+    guitardsp::hq::HQEffectsRack& rack;
+
     juce::Label title;
-    juce::Label info;
+    juce::Label modTitle, delayTitle, reverbTitle;
+    juce::Label modHint, delayHint, reverbHint;
+
+    juce::ComboBox modType;
+    juce::Slider modRate, modDepth, modMix, modFeedback, modManual, modShape;
+
+    juce::ToggleButton delayEnabled { "DELAY ON" };
+    juce::ComboBox delayType;
+    juce::Slider delayTime, delayFeedback, delayMix, delayLowCut, delayHighCut, delayDrive, delayWow, delayFlutter, delayAge;
+
+    juce::ToggleButton reverbEnabled { "REVERB ON" };
+    juce::ComboBox reverbType;
+    juce::Slider reverbSize, reverbDecay, reverbDamping, reverbPreDelay, reverbMix, reverbMod, reverbDrip;
 };
