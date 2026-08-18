4286118e08b237c71cef30ca9e440a2a6d738a74
puyan3939 <puyan3939@gmail.com>
2026-08-18 20:00:59 +0900

    wire pedal rack UI controls

--- ui/pages/PedalPage.h
+++ ui/pages/PedalPage.h
@@ -1,15 +1,36 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "../../hq_preload/dsp/HQEffectsRack.h"
 
 class PedalPage : public juce::Component
 {
 public:
-    PedalPage();
+    explicit PedalPage(guitardsp::hq::HQEffectsRack& rack);
     void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
+    void loadSlot();
+    void pushControls();
+    void updateModelLabels();
+    void setupKnob(juce::Slider& slider, const juce::String& name,
+                   double min, double max, double step);
+
+    guitardsp::hq::HQEffectsRack& effectsRack;
+    bool updating = false;
+
     juce::Label title;
-    juce::Label info;
+    juce::Label description;
+    juce::ComboBox slotSelector;
+    juce::ComboBox modelSelector;
+    juce::ToggleButton enabledButton { "ON" };
+
+    juce::Slider drive;
+    juce::Slider tone;
+    juce::Slider level;
+    juce::Slider blend;
+    juce::Slider aux1;
+    juce::Slider aux2;
+    juce::Slider aux3;
 };
