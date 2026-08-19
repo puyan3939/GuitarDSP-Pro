3d3269c329c84b35eb341ee8f2bb96658039c95f
puyan3939 <puyan3939@gmail.com>
2026-08-19 14:53:18 +0900

    add analyzer drawer toggle

--- src/MainComponent.cpp
+++ src/MainComponent.cpp
@@ -6,6 +6,21 @@ MainComponent::MainComponent()
     setOpaque(true);
     setSize(1280, 720);
     addAndMakeVisible(mainView);
+    addAndMakeVisible(analyzerButton);
+    addChildComponent(analyzerPanel);
+
+    analyzerButton.setColour(juce::TextButton::buttonColourId, juce::Colour::fromRGB(26, 31, 38));
+    analyzerButton.setColour(juce::TextButton::textColourOffId, juce::Colour::fromRGB(222, 110, 58));
+    analyzerButton.setTooltip("Open dual oscilloscope / FFT analyzer");
+    analyzerButton.onClick = [this]
+    {
+        const bool shouldShow = !analyzerPanel.isVisible();
+        analyzerPanel.setVisible(shouldShow);
+        analyzerButton.setButtonText(shouldShow ? "CLOSE ANALYZER" : "ANALYZER");
+        analyzerPanel.toFront(false);
+        analyzerButton.toFront(false);
+        resized();
+    };
 }
 
 MainComponent::~MainComponent()
@@ -21,6 +36,33 @@ void MainComponent::paint(juce::Graphics& g)
 void MainComponent::resized()
 {
     mainView.setBounds(getLocalBounds());
+
+    constexpr int panelHeight = 360;
+    constexpr int buttonWidth = 126;
+    constexpr int buttonHeight = 28;
+    constexpr int margin = 10;
+
+    if (analyzerPanel.isVisible())
+    {
+        analyzerPanel.setBounds(margin,
+                                juce::jmax(margin, getHeight() - panelHeight - margin),
+                                juce::jmax(200, getWidth() - margin * 2),
+                                juce::jmin(panelHeight, getHeight() - margin * 2));
+        analyzerButton.setBounds(getWidth() - buttonWidth - margin,
+                                 juce::jmax(margin, analyzerPanel.getY() - buttonHeight - 5),
+                                 buttonWidth,
+                                 buttonHeight);
+    }
+    else
+    {
+        analyzerButton.setBounds(getWidth() - buttonWidth - margin,
+                                 getHeight() - buttonHeight - margin,
+                                 buttonWidth,
+                                 buttonHeight);
+    }
+
+    analyzerPanel.toFront(false);
+    analyzerButton.toFront(false);
 }
 
 bool MainComponent::keyPressed(const juce::KeyPress& key)
