4f0c1e8b42e49b7e50551d870ba5beb15aedc987
puyan3939 <puyan3939@gmail.com>
2026-08-20 07:21:27 +0900

    Add HQ compressor controls to pedal page

--- ui/pages/PedalPage.h
+++ ui/pages/PedalPage.h
@@ -3,33 +3,25 @@
 #include <JuceHeader.h>
 #include "../../hq_preload/dsp/HQEffectsRack.h"
 
-class PedalPage : public juce::Component
+class PedalPage : public juce::Component,
+                  private juce::Timer
 {
 public:
     explicit PedalPage(guitardsp::hq::HQEffectsRack& rack);
+    ~PedalPage() override { stopTimer(); }
     void paint(juce::Graphics&) override;
     void resized() override;
-    void refreshFromEngine()
-    {
-        auto& gc=effectsRack.gateControl();
-        gateThreshold.setValue(gc.thresholdDb.load(),juce::dontSendNotification);
-        gateRange.setValue(gc.rangeDb.load(),juce::dontSendNotification);
-        gateRatio.setValue(gc.ratio.load(),juce::dontSendNotification);
-        gateAttack.setValue(gc.attackMs.load(),juce::dontSendNotification);
-        gateHold.setValue(gc.holdMs.load(),juce::dontSendNotification);
-        gateRelease.setValue(gc.releaseMs.load(),juce::dontSendNotification);
-        gateHysteresis.setValue(gc.hysteresisDb.load(),juce::dontSendNotification);
-        gateSidechainHp.setValue(gc.sidechainHpHz.load(),juce::dontSendNotification);
-        gateSidechainLp.setValue(gc.sidechainLpHz.load(),juce::dontSendNotification);
-        gateEnabled.setToggleState(effectsRack.getDynamicsMode()==guitardsp::hq::HQEffectsRack::DynamicsMode::gate,juce::dontSendNotification);
-        loadSlot();
-    }
+    void refreshFromEngine();
 
 private:
+    void timerCallback() override;
     void loadSlot();
     void pushControls();
     void pushGateControls();
+    void pushStudioCompControls();
+    void pushGuitarCompControls();
     void updateModelLabels();
+    void updateDynamicsVisibility();
     void setupKnob(juce::Slider& slider, const juce::String& name,
                    double min, double max, double step);
 
@@ -42,9 +34,18 @@ private:
     juce::ComboBox modelSelector;
     juce::ToggleButton enabledButton { "ON" };
 
-    juce::ToggleButton gateEnabled { "PRECISION GATE" };
+    juce::Label dynamicsTitle, dynamicsHint, grLabel;
+    juce::ComboBox dynamicsMode;
     juce::Slider gateThreshold, gateRange, gateRatio, gateAttack, gateHold,
                  gateRelease, gateHysteresis, gateSidechainHp, gateSidechainLp;
+
+    juce::Slider compThreshold, compRatio, compAttack, compRelease,
+                 compKnee, compMakeup, compMix, compSidechainHp;
+    juce::ToggleButton compRms { "RMS" };
+    juce::ToggleButton compAutoRelease { "AUTO REL" };
+    juce::ToggleButton compAutoMakeup { "AUTO MAKEUP" };
+
+    juce::Slider guitarSustain, guitarAttack, guitarBlend, guitarLevel;
 
     juce::Slider drive;
     juce::Slider tone;
