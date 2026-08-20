38cbbc3f56d2230526204f2f53b4cf33c0f98102
puyan3939 <puyan3939@gmail.com>
2026-08-21 02:41:40 +0900

    Expose complete CLEAN SUB alignment controls in RIG UI

--- ui/pages/PerformancePage.h
+++ ui/pages/PerformancePage.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <JuceHeader.h>
 #include "../../engine/AudioEngine.h"
 
@@ -10,38 +9,17 @@ public:
     void paint(juce::Graphics&) override;
     void resized() override;
     void refreshFromEngine();
-
 private:
-    void setupKnob(juce::Slider&, const juce::String& name, double lo, double hi, double value, double step, const juce::String& suffix = {});
-    void pushInput();
-    void pushPitch();
-    void pushRigDetail();
-    void pushDualDelay();
-    void captureScene();
-    void recallScene();
-
-    AudioEngine& engine;
-    bool updating = false;
-
-    juce::Label title;
-    juce::Label inputTitle, pitchTitle, rigTitle, delayTitle, sceneTitle;
-
-    juce::ToggleButton inputEnabled { "INPUT LOAD" };
-    juce::Slider pickupR, pickupL, cableC, inputZ, inputTrim;
-
-    juce::ToggleButton pitchEnabled { "WHAMMY" };
-    juce::ToggleButton pitchMain { "MAIN" }, pitchClean { "CLEAN" }, pitchSub { "SUB" };
-    juce::Slider pitchSemitones, pitchExpression, pitchWet, pitchDry, pitchTracking, pitchTone, pitchSmooth;
-
-    juce::ToggleButton rigEnabled { "MULTI-RIG" };
-    juce::ToggleButton cleanEnabled { "CLEAN" }, subEnabled { "SUB" }, autoLatency { "AUTO LATENCY" };
-    juce::Slider mainLevel, cleanLevel, subLevel, cleanBass, cleanMid, cleanTreble, subBass, subMid, subTreble;
-
-    juce::ToggleButton dualDelayEnabled { "DUAL DELAY" };
-    juce::Slider delayL, delayR, feedbackL, feedbackR, crossFeedback, delayMix, delayLowCut, delayHighCut, delayModRate, delayModDepth;
-
-    juce::ComboBox outputMode;
-    juce::ComboBox sceneSelector;
-    juce::TextButton sceneCapture { "CAPTURE" };
-    juce::TextButton sceneRecall { "RECALL" };
+    void setupKnob(juce::Slider&,const juce::String&,double,double,double,double,const juce::String& ={});
+    void pushInput(); void pushPitch(); void pushRigDetail(); void pushDualDelay(); void captureScene(); void recallScene();
+    AudioEngine& engine; bool updating=false;
+    juce::Label title,inputTitle,pitchTitle,rigTitle,delayTitle,sceneTitle;
+    juce::ToggleButton inputEnabled{"INPUT LOAD"}; juce::Slider pickupR,pickupL,cableC,inputZ,inputTrim;
+    juce::ToggleButton pitchEnabled{"WHAMMY"},pitchMain{"MAIN"},pitchClean{"CLEAN"},pitchSub{"SUB"};
+    juce::Slider pitchSemitones,pitchExpression,pitchWet,pitchDry,pitchTracking,pitchTone,pitchSmooth;
+    juce::ToggleButton rigEnabled{"MULTI-RIG"},cleanEnabled{"CLEAN"},subEnabled{"SUB"},autoLatency{"AUTO LATENCY"},cleanInvert{"CLEAN INV"},subInvert{"SUB INV"};
+    juce::Slider mainLevel,mainDelay,cleanLevel,cleanHp,cleanLp,cleanPresence,cleanDrive,cleanBass,cleanMid,cleanTreble,cleanDelay;
+    juce::Slider subLevel,subHp,subLp,subBody,subDrive,subBass,subMid,subTreble,subTracking,subTone,subSmooth,subDelay;
+    juce::ToggleButton dualDelayEnabled{"DUAL DELAY"}; juce::Slider delayL,delayR,feedbackL,feedbackR,crossFeedback,delayMix,delayLowCut,delayHighCut,delayModRate,delayModDepth;
+    juce::ComboBox outputMode,sceneSelector; juce::TextButton sceneCapture{"CAPTURE"},sceneRecall{"RECALL"};
 };
