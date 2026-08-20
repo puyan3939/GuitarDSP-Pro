7ba0a3b967bb823513b920bbaeed49cf0e262259
puyan3939 <puyan3939@gmail.com>
2026-08-21 01:14:24 +0900

    Add per-pedal route masks and independent bus DSP state

--- hq_preload/dsp/HQEffectsRack.h
+++ hq_preload/dsp/HQEffectsRack.h
@@ -18,6 +18,9 @@ class HQEffectsRack {
 public:
     static constexpr int pedalSlots = 4;
     static constexpr int stereoChannels = 2;
+    static constexpr int routeMain = 1;
+    static constexpr int routeClean = 2;
+    static constexpr int routeSub = 4;
     enum class DynamicsMode { off, gate, studioCompressor, guitarCompressor };
     enum class ModulationMode { off, chorus, flanger, phaser, tremolo, vibrato };
 
@@ -26,12 +29,10 @@ public:
         std::atomic<int> model{(int)PedalType::midOD};
         std::atomic<float> drive{0.5f}, tone{0.5f}, levelDb{0.0f}, mix{1.0f};
         std::atomic<float> aux1{0.5f}, aux2{0.5f}, aux3{0.5f};
+        // Bit mask: MAIN=1, CLEAN=2, SUB=4. Multiple destinations are allowed.
+        std::atomic<int> routeMask{routeMain};
     };
-    struct GateControl {
-        std::atomic<float> thresholdDb{-55}, rangeDb{-60}, ratio{4};
-        std::atomic<float> attackMs{1}, holdMs{35}, releaseMs{180}, hysteresisDb{4};
-        std::atomic<float> sidechainHpHz{55}, sidechainLpHz{6500};
-    };
+    struct GateControl { std::atomic<float> thresholdDb{-55}, rangeDb{-60}, ratio{4}; std::atomic<float> attackMs{1}, holdMs{35}, releaseMs{180}, hysteresisDb{4}; std::atomic<float> sidechainHpHz{55}, sidechainLpHz{6500}; };
     struct StudioCompControl { std::atomic<float> thresholdDb{-18}, ratio{4}, attackMs{10}, releaseMs{120}, kneeDb{6}, makeupDb{0}, mix{1}; std::atomic<bool> rms{true}; };
     struct GuitarCompControl { std::atomic<float> sustain{.55f}, attack{.45f}, blend{.8f}, levelDb{0}; };
     struct ModulationControl { std::atomic<float> rateHz{.7f}, depth{.5f}, mix{.5f}, feedback{0}, manual{.5f}, shape{.5f}; };
@@ -41,6 +42,7 @@ public:
     void prepare(double sampleRate, int maximumBlockSize);
     void reset();
     void processPreAmp(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
+    void processPedalRoute(juce::AudioBuffer<float>& buffer, int startSample, int numSamples, int routeBit);
     void processPostAmp(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
 
     PedalSlotControl& pedalSlot(int index) noexcept { return pedalControls[(size_t)juce::jlimit(0, pedalSlots - 1, index)]; }
@@ -63,40 +65,22 @@ public:
     bool isReverbEnabled() const noexcept { return reverbEnabled.load(std::memory_order_relaxed); }
 
 private:
+    static int routeIndex(int routeBit) noexcept { return routeBit==routeClean ? 1 : routeBit==routeSub ? 2 : 0; }
     void applyDynamics(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
     void applyModulation(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
     void updateDynamicParameters();
     void updatePostParameters();
 
-    std::array<std::array<PedalEngineHQ, pedalSlots>, stereoChannels> pedals;
+    // Independent state per route is required for nonlinear/stateful pedals in parallel paths.
+    std::array<std::array<std::array<PedalEngineHQ, pedalSlots>, stereoChannels>, 3> routePedals;
     std::array<PedalSlotControl, pedalSlots> pedalControls;
     std::array<NoiseGate, stereoChannels> noiseGate;
     std::array<StudioCompressor, stereoChannels> studioComp;
     std::array<GuitarCompressor, stereoChannels> guitarComp;
-    GateControl gateControls;
-    StudioCompControl studioControls;
-    GuitarCompControl guitarControls;
-    std::array<ChorusHQ, stereoChannels> chorus;
-    std::array<FlangerHQ, stereoChannels> flanger;
-    std::array<PhaserHQ, stereoChannels> phaser;
-    std::array<TremoloHQ, stereoChannels> tremolo;
-    std::array<VibratoHQ, stereoChannels> vibrato;
-    ModulationControl modControls;
-    std::array<DelayHQ, stereoChannels> delayFx;
-    std::array<ReverbHQ, stereoChannels> reverbFx;
-    DelayControl delayControls;
-    ReverbControl reverbControls;
-    ParallelRigControl parallelControls;
-
-    // Realtime work buffers are allocated once in prepare(). Never allocate on the audio thread.
-    juce::AudioBuffer<float> pedalMonoWork;
-    juce::AudioBuffer<float> gateCleanKey;
-    int preparedMaxBlock = 0;
-
-    std::atomic<int> dynamicsMode{(int)DynamicsMode::off};
-    std::atomic<int> modulationMode{(int)ModulationMode::off};
-    std::atomic<bool> delayEnabled{false};
-    std::atomic<bool> reverbEnabled{false};
+    GateControl gateControls; StudioCompControl studioControls; GuitarCompControl guitarControls;
+    std::array<ChorusHQ, stereoChannels> chorus; std::array<FlangerHQ, stereoChannels> flanger; std::array<PhaserHQ, stereoChannels> phaser; std::array<TremoloHQ, stereoChannels> tremolo; std::array<VibratoHQ, stereoChannels> vibrato;
+    ModulationControl modControls; std::array<DelayHQ, stereoChannels> delayFx; std::array<ReverbHQ, stereoChannels> reverbFx; DelayControl delayControls; ReverbControl reverbControls; ParallelRigControl parallelControls;
+    juce::AudioBuffer<float> pedalMonoWork; juce::AudioBuffer<float> gateCleanKey; int preparedMaxBlock = 0;
+    std::atomic<int> dynamicsMode{(int)DynamicsMode::off}; std::atomic<int> modulationMode{(int)ModulationMode::off}; std::atomic<bool> delayEnabled{false}; std::atomic<bool> reverbEnabled{false};
 };
-
 }
