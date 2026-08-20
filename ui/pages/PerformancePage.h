d4cb02d0128e57fbd1d968de0d87ed72ea6d8534
puyan3939 <puyan3939@gmail.com>
2026-08-21 01:20:07 +0900

    Add performance rig control page for multi-amp pitch scenes delay outputs and input loading

---
+++ ui/pages/PerformancePage.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "../../engine/AudioEngine.h"
+
+class PerformancePage : public juce::Component
+{
+public:
+    explicit PerformancePage(AudioEngine& engine);
+    void paint(juce::Graphics&) override;
+    void resized() override;
+    void refreshFromEngine();
+
+private:
+    void setupKnob(juce::Slider&, const juce::String& name, double lo, double hi, double value, double step, const juce::String& suffix = {});
+    void pushInput();
+    void pushPitch();
+    void pushRigDetail();
+    void pushDualDelay();
+    void captureScene();
+    void recallScene();
+
+    AudioEngine& engine;
+    bool updating = false;
+
+    juce::Label title;
+    juce::Label inputTitle, pitchTitle, rigTitle, delayTitle, sceneTitle;
+
+    juce::ToggleButton inputEnabled { "INPUT LOAD" };
+    juce::Slider pickupR, pickupL, cableC, inputZ, inputTrim;
+
+    juce::ToggleButton pitchEnabled { "WHAMMY" };
+    juce::ToggleButton pitchMain { "MAIN" }, pitchClean { "CLEAN" }, pitchSub { "SUB" };
+    juce::Slider pitchSemitones, pitchExpression, pitchWet, pitchDry, pitchTracking, pitchTone, pitchSmooth;
+
+    juce::ToggleButton autoLatency { "AUTO LATENCY" };
+    juce::Slider cleanBass, cleanMid, cleanTreble, subBass, subMid, subTreble;
+
+    juce::ToggleButton dualDelayEnabled { "DUAL DELAY" };
+    juce::Slider delayL, delayR, feedbackL, feedbackR, crossFeedback, delayMix, delayLowCut, delayHighCut, delayModRate, delayModDepth;
+
+    juce::ComboBox outputMode;
+    juce::ComboBox sceneSelector;
+    juce::TextButton sceneCapture { "CAPTURE" };
+    juce::TextButton sceneRecall { "RECALL" };
+};
