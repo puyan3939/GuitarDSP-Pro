a27ffee03edd80d5d623bc2aee3a75ac86e51621
puyan3939 <puyan3939@gmail.com>
2026-08-20 09:20:50 +0900

    prepare analyzer for separate window

--- src/MainComponent.h
+++ src/MainComponent.h
@@ -3,7 +3,6 @@
 #include <JuceHeader.h>
 #include "../engine/AudioEngine.h"
 #include "../ui/MainView.h"
-#include "../ui/SignalAnalyzerPanel.h"
 
 class MainComponent : public juce::Component
 {
@@ -16,10 +15,15 @@ public:
     bool keyPressed(const juce::KeyPress& key) override;
 
 private:
+    class AnalyzerWindow;
+
+    void toggleAnalyzerWindow();
+    void setAnalyzerWindowVisible(bool shouldShow);
+
     AudioEngine audioEngine;
     MainView mainView { audioEngine };
-    SignalAnalyzerPanel analyzerPanel { audioEngine };
     juce::TextButton analyzerButton { "ANALYZER" };
+    std::unique_ptr<AnalyzerWindow> analyzerWindow;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
